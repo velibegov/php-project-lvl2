6569862631ec4d62725f7ecacf7b005685c0fac5
Ruslan <velibegov@mail.ru>
2021-04-21 20:06:53 +0000

    Changes to be committed: 	modified:   src/Formatters/Plain.php

--- src/Formatters/Plain.php
+++ src/Formatters/Plain.php
@@ -25,35 +25,33 @@ function toString($value): string
  */
 function format(array $differenceTree, array $parts = []): string
 {
-    $mapped = array_map(
-        function ($value) use ($parts): string {
-            $result = '';
-            $propertyNameParts = collect($parts);
-            $merged = $propertyNameParts->merge($value['key']);
-            $propertyName = implode('.', $merged->all());
-            switch ($value['type']) {
-                case 'parent':
-                    return format($value['children'], $merged->all());
-                case 'modified':
-                    $oldValue = toString($value['old']);
-                    $newValue = toString($value['new']);
-                    $result = "Property '{$propertyName}' was updated. From {$oldValue} to {$newValue}";
-                    break;
-                case 'removed':
-                    $result = "Property '{$propertyName}' was removed";
-                    break;
-                case 'added':
-                    $added = toString($value['value']);
-                    $result = "Property '{$propertyName}' was added with value: {$added}";
-                    break;
-                case 'unmodified':
-                    break;
-                default:
-                    throw new Exception('Unknown value type ' . $value['type']);
-            }
-            return $result;
-        },
-        $differenceTree
-    );
+    $mapped = array_map(function ($value) use ($parts) {
+        $propertyNameParts = collect($parts);
+        $merged = $propertyNameParts->merge($value['key']);
+        $propertyName = implode('.', $merged->all());
+        switch ($value['type']) {
+            case 'parent':
+                return format($value['children'], $merged->all());
+            case 'modified':
+                $oldValue = toString($value['old']);
+                $newValue = toString($value['new']);
+                $result = "Property '{$propertyName}' was updated. From {$oldValue} to {$newValue}";
+                break;
+            case 'removed':
+                $result = "Property '{$propertyName}' was removed";
+                break;
+            case 'added':
+                $added = toString($value['value']);
+                $result = "Property '{$propertyName}' was added with value: {$added}";
+                break;
+            case 'unmodified':
+                $result = null;
+                break;
+            default:
+                throw new Exception('Unknown value type ' . $value['type']);
+        }
+        return $result;
+    },
+        $differenceTree);
     return implode("\n", array_values(array_filter($mapped)));
 }
